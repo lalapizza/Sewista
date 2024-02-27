ebadeb8cd0afd33147c72f01e62d990e092ca5c0
Ngô Xuân Minh <xminh.ngo.00@gmail.com>
2024-02-28 00:21:53 +0700

    chore: Bootstrap project with spring initialzr

---
+++ src/main/java/com/lalapizza/sewista/SewistaApplication.java
@@ -0,0 +1,13 @@
+package com.lalapizza.sewista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SewistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SewistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lalapizza/sewista/SewistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lalapizza.sewista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SewistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
